f894745988485fda5dce1e94af67cd13c7ffa958
Lizhuofeng <lzf22@mails.tsinghua.edu.cn>
2023-04-15 09:07:26 +0800

    feature_extract

--- feature_extract.py
+++ feature_extract.py
@@ -9,34 +9,41 @@ import radiomics
 from radiomics import featureextractor
 import SimpleITK as sitk  
 
-od.chdir("/data1/lzf/STAS/gdph/Code_radiomics")
+os.chdir("/data1/lzf/STAS/gdph/Code_radiomics")
 
 para_path = "./config/feature.yaml"
 extractor = featureextractor.RadiomicsFeatureExtractor(para_path) 
 image_folder_path = "../original_image/processed_nii/"
 seg_folder_path = "../original_image/segmentation/"
+split = "test"
 
-with open("./config/train_pos_id.txt", "r") as f:
+# get patients' id
+with open("./config/"+split+"_pos_id.txt", "r") as f:
     pos_id = eval(f.read())
-with open("./config/train_neg_id.txt", "r") as f:
+with open("./config/"+split+"_neg_id.txt", "r") as f:
     neg_id = eval(f.read())
 print(len(neg_id), len(pos_id))
 
 id_all = {"pos": pos_id, "neg": neg_id}
 
-for split in id_all:
-    print("extract "+split+" feature.")
+for pos_or_neg in id_all:
+    print("extract "+pos_or_neg+" feature.")
     features_dict = dict()
     df = pd.DataFrame()
-    for index, path in enumerate(id_all[split]):
-        image_path = os.path.join(image_folder_path, path+".nii.gz")
-        seg_path = os.path.join(seg_folder_path, path+".nii.gz")
-        features = extractor.execute(imageFilepath=image_path, maskFilepath=seg_path)
-        features_dict['index'] = path
-        for feature_key, feature_value in features.items():  #输出特征
-            features_dict[feature_key] = feature_value
-        df = pd.concat([df, pd.DataFrame.from_dict(features_dict, orient='index').T], axis=0, ignore_index=True)
+    for index, path in enumerate(id_all[pos_or_neg]):
+        try:
+            # extract features and store in df
+            image_path = os.path.join(image_folder_path, path+".nii.gz")
+            seg_path = os.path.join(seg_folder_path, path+".nii.gz")
+            features = extractor.execute(imageFilepath=image_path, maskFilepath=seg_path)
+            features_dict['index'] = path
+            for feature_key, feature_value in features.items():
+                features_dict[feature_key] = feature_value
+            df = pd.concat([df, pd.DataFrame.from_dict(features_dict, orient='index').T], axis=0, ignore_index=True)
+        except Exception as e:
+            with open("./config/except.txt", "a") as f:
+                f.write(str(e))
     df.columns = features_dict.keys()
-    df.to_csv("./feature/"+split+"_feature.csv",index=0)
+    df.to_csv("./feature/"+split+"_"+pos_or_neg+"_feature.csv",index=0)
     
         
